cc70f6ec8111f4b86018e521ce785810285b8553
Muhammad Afan Aljafar <58525588+AfanAljafar@users.noreply.github.com>
2023-10-12 23:40:38 +0700

    Delete portofolio-project/buzz directory

--- portofolio-project/buzz/components/group/footer/index.js
+++
@@ -1,9 +0,0 @@
-import "./Footer.css";
-
-export default function Footer() {
-  return (
-    <div className="footer">
-      <div className="tribute">Designed by @AfanAljafar</div>
-    </div>
-  );
-}
